2b7788335d23bea39a8185f46b69d5a2414153da
Divya Prakash <divyaprakashara09@gmail.com>
2025-10-25 09:40:11 +0530

    pandas dataframe methodss

---
+++ Pandas_Learning/Pandas_DataFrame/pandasdataframe2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+movies = pd.read_csv('Pandas_Learning\\Pandas_DataFrame\\imdb-top-1000.csv')
+deliveries = pd.read_csv('Pandas_Learning\\Pandas_DataFrame\\deliveries.csv')
+# print(movies)
+# print(deliveries)
